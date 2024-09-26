7e3695a92f734aeb8c486e7e1e701051925091ad
Charbel Sarkis <charbelsako>
2024-09-26 19:14:36 +0300

    Fixes environment values to new server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@
 export const environment = {
     production: true,
     APP_SECRET_KEY: "123456789123456789",
-    backendUrl:'http://52.72.41.246:3000/api/v1',
-    socket_url:'http://52.72.41.246:3000',
+    backendUrl:'http://192.236.178.57:3000/api/v1',
+    socket_url:'http://192.236.178.57:3000',
     googleMapAPIKEY: 'AIzaSyBiUlq5hbCyUnzcXIujbcKUWa683FFYgAs'
 };
 
