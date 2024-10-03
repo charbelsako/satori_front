58ef7b5aa0b8ce4d4bbefe8d6a9f390345477a77
Charbel Sarkis <charbelsako>
2024-10-03 18:36:32 +0300

    Builds to use port 5000

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@ export const environment = {
     production: false,
     APP_SECRET_KEY: "123456789123456789",
     // @TODO: for development only
-    backendUrl:'http://localhost:3000/api/v1',
-    socket_url:'http://localhost:3000',
+    backendUrl:'http://localhost:5000/api/v1',
+    socket_url:'http://localhost:5000',
     // backendUrl:'http://192.236.178.57:3000/api/v1',
     // socket_url:'http://192.236.178.57:3000',
 };
